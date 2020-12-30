bf8a15cc5564cc297a86809f2f07bba57e83ba5b
venkatesh <pvenkateshnaidu@gmail.com>
2020-12-30 19:09:07 +0530

    Bug fix in Email

--- resources/views/email_template.blade.php
+++ resources/views/email_template.blade.php
@@ -1,9 +1,3 @@
-<!--
-    /resources/views/email_template.blade.php
--->
-@php
- $datetime = date("Y/m/d h:i:sa")
-@endphp
-<h1>Hey, how you doing bro?</h1>
-{{$datetime}}
-I have some info for you, please visit my website  <a href="http://webmobilez.com">here</a>.
+
+
+{{$message}}
