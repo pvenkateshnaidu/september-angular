3ad404f2ddfea3fcf178fd6a7057fb771e99dcaf
venkatesh <pvenkateshnaidu@gmail.com>
2021-02-04 09:51:50 +0530

    Added Technologies

---
+++ common-es2015.ac7b40e1a1ef4c8423f1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{SVXH:function(n,t,u){"use strict";u.d(t,"a",function(){return r}),u.d(t,"b",function(){return a});var c=u("8Y7J");u("7LN8"),u("SVse");var r=c["\u0275crt"]({encapsulation:2,styles:[],data:{}});function a(n){return c["\u0275vid"](0,[c["\u0275ncd"](null,0)],null,null)}}}]);
